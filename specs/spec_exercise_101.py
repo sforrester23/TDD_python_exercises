cef3b85c5e2ec77ec224a7f864d7dfc4f8f4bfc1
Filipe-p <filipe.p@soltroiavillas.com>
2019-05-28 12:17:43 +0100

    created 6 tests

--- specs/spec_exercise_101.py
+++ specs/spec_exercise_101.py
@@ -1,1 +1,13 @@
-from py_excercises_test_101.specs.exercise_101 import *
+from exercise_101 import *
+
+def test_hello_human1():
+    assert hello_human('Jefferson') == 'Hello Jefferson, you are a humman'
+
+def test_hello_human2():
+    assert hello_human('Joana') == 'Hello Joana, you are a humman'
+
+def test_area_square():
+    assert area_square(10) == 100
+
+def test_area_triangle():
+    assert area_triangle(10, 10) == 50
