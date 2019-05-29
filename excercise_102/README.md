25b660907409d3decd05ebbeced6175d7507c118
Filipe-p <filipe.p@soltroiavillas.com>
2019-05-29 09:52:27 +0100

    wip for excercise 2

---
+++ excercise_102/README.md
@@ -0,0 +1,24 @@
+# Python  exercise 102 :taco:
+
+The excercises come with two main files: 
+ - exercise file
+ - specs file
+ 
+The object is you keep running the tests until you pass! :happy:
+
+Let's get started. First clone this repo by running:
+
+
+    $ git clone <ssh of this repo>
+
+`cd` into the right directory.
+Then what you are going to do is run the tests:
+
+    $ pytest exercise_102.py00
+
+
+Inside each exercise file you have some methods to complete.
+
+Read the comment for guidance & run your tests to know that you're done.
+
+Good luck :) 
---
+++ excercise_102/specs_102.py
@@ -0,0 +1,63 @@
+from exercise_102 import *
+
+def test_full_name1():
+    assert full_name('Jefferson', 'Yunn') == 'Jefferson Yunn'
+
+def test_full_name2():
+    assert full_name('andreia', 'MORALES') == 'Andreia Morales'
+
+
+def test_calculator_sum():
+    assert calculator_sum(2, 4) == 6
+
+def test_calculator_sum2():
+    assert calculator_sum(10, 44) == 54
+
+def test_calculator_sum3():
+    assert calculator_sum("2", 4) == 6
+
+def test_calculator_sum4():
+    assert calculator_sum("2", "44") == 46
+
+
+def test_calculator_subtract():
+    assert calculator_sum(20, 4) == 16
+
+
+def test_calculator_subtract2():
+    assert calculator_sum(10, 4) == 6
+
+
+def test_calculator_subtract3():
+    assert calculator_sum("20", 4) == 16
+
+
+def test_calculator_subtract4():
+    assert calculator_sum("2", "44") == -42
+
+def test_calculator_multiply():
+    assert calculator_sum(20, 4) == 60
+
+
+def test_calculator_multiply2():
+    assert calculator_sum(10, 4) == 40
+
+
+def test_calculator_multiply3():
+    assert calculator_sum("22", 4) == 68
+
+
+def test_calculator_multiply4():
+    assert calculator_sum("2", "44") == 48
+
+def test_area_square():
+    assert area_square(10) == 100
+
+def test_area_square1():
+    assert area_square('13') == 169
+
+def test_area_triangle():
+    assert area_triangle(10, 10) == 50
+
+def test_area_triangle2():
+    assert area_triangle('10', '13') == 65
